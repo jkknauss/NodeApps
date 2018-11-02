8273d1b3a992210a4fd7f659f36864086d687ce5
jkknauss <44242189+jkknauss@users.noreply.github.com>
2018-11-02 10:19:07 -0500

    practice midterm

---
+++ App/Class Project/Midterm Practice/app/models/todos.js
@@ -0,0 +1,12 @@
+var Mongoose = require('mongoose');
+var Schema = Mongoose.Schema;
+
+var todoSchema = new Schema({
+    todo: { type: String, require: true },
+    priority: { type: String, require: true, enum: ['Critical', 'High', 'Medium', 'Low'] },
+    dateDue: { type: Date, default: Date.now },
+
+});
+
+module.exports =
+    Mongoose.model('Todo', todoSchema);
--- App/Class Project/Midterm Practice/index.js
+++ App/Class Project/Midterm Practice/index.js
@@ -1,1 +1,13 @@
-hfdjklsa;
+var express = require('express');
+var config = require('./config/config');
+var logger = require('./config/logger'); 
+
+var app = express();
+
+require('./config/express')(app, config);
+
+require('http').createServer(app).listen(config.port, function () {
+    logger.log('info', "HTTP Server listening on port: %d, in %s mode", config.port, app.get('env'));
+});
+
+module.exports = app;
